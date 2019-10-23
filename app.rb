cb500b1e4d540f9356341c78160c7081580692f4
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-10-23 18:36:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   
   get '/say/:number/:phrase' do
     @say = params[:phrase].to_s
-    "#{@say}".times(params[:number])
+    @say.times(params[:number])
   end
   
   get '/say/:word1/:word2/:word3/:word4/:word5' do
