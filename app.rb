1293200288d3096932a26f43ab061a567c25f5d9
Giancarlo Zabaleta <gzabaleta48@gmail.com>
2019-10-23 18:45:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,5 +28,6 @@ class App < Sinatra::Base
       params[:number1].to_i - params[:number2].to_i
     elsif params[:operation] = "multiply"
       params[:number1].to_i * params[:number2].to_i
+    end
   end
 end
